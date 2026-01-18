de6db81caa51c7f7075b2fec4ca02a6df16ff822
lucasrib421 <lucasrib421@gmail.com>
2026-01-18 20:14:54 -0300

    Configuração de Produção: Traefik, Gunicorn e Whitenoise

---
+++ homeNews/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+
+app_name = "homeNews"
+
+urlpatterns = [
+
+    #path(""),
+    #3path("carrosel/", views.carrosel, name="carrosel"),
+
+]
