8d84e34681db0aff056722d773d69fd580b2b7ec
Paulo Filipe Moreira da Silva <filipemsilva@outlook.com.br>
2026-02-08 22:45:17 -0300

    feat(admin): customize Django admin list views and inlines

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 
 from accounts.models import Author, Role
 
-admin.site.register(Role)
-admin.site.register(Author)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'role', 'user')
+    list_filter = ('role',)
+    search_fields = ('name', 'bio')
+
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name',)
--- home/admin.py
+++ home/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 
 from home.models import HomeSection, HomeSectionItem
 
-admin.site.register(HomeSection)
-admin.site.register(HomeSectionItem)
+
+class HomeSectionItemInline(admin.TabularInline):
+    model = HomeSectionItem
+    extra = 1
+
+
+@admin.register(HomeSection)
+class HomeSectionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'section_type', 'order', 'is_active')
+    list_filter = ('section_type', 'is_active')
+    search_fields = ('title',)
+    inlines = [HomeSectionItemInline]
+
+
+@admin.register(HomeSectionItem)
+class HomeSectionItemAdmin(admin.ModelAdmin):
+    list_display = ('section', 'post', 'order')
+    list_filter = ('section',)
